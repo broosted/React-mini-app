68877d766ddf7317091ab7e77bd1b93e49c9fe14
broosted <matrix.10.tad@gmail.com>
2017-12-09 14:48:34 -0800

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -4,14 +4,15 @@ import {connect} from 'react-redux';
 import {removeExpense} from '../actions/expenses.jsx';
 import selectExpenses from '../selectors/expenses.jsx';
 import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, note, amount, createdAt, props}) =>  (
    <li>Description:
        <Link to={`/edit/${id}`} >{description}</Link> 
        <p>Note: {note}</p>
-       <p>Amount: {amount}</p>
-       <p>CreatedAt: {createdAt}</p>
+       <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
+       <p>CreatedAt: {moment(createdAt).format('MMMM Do, YYYY')}</p>
        <button onClick={() => {
            props.dispatch(removeExpense({id}));
        }}>Remove</button>
